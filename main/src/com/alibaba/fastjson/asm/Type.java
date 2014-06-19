ca440d25e762e75833b23bb09e9aa1770227b360
donyang <donyangok@gmail.com>
2014-06-19 23:32:32 +0800

    add fast json

--- main/src/com/alibaba/fastjson/asm/Type.java
+++
@@ -1,6 +0,0 @@
-// INTERNAL ERROR //
-
-/* Location:           /Users/don/DeSources/alipay/backup/zhifubaoqianbao_52/classes-dex2jar.jar
- * Qualified Name:     com.alibaba.fastjson.asm.Type
- * JD-Core Version:    0.6.2
- */
